ed71e204ca4cecaf19cbe384f0286f21760ee4fe
Gabriel Rocha <shadowninja2093@gmail.com>
2023-09-12 12:05:34 -0300

    utilizando a forma mais atualizada de criar rotas e usando hooks no exemplo1

---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,15 @@
+import { } from 'react'
+import { Link } from 'react-router-dom'
+
+function Nav() {
+
+  return (
+    <>
+    <Link to="produtos">Produtos</Link>
+    <Link to="exemplo1">Exemplo1</Link>
+    <Link to="/">Home</Link>
+    </>
+  )
+}
+
+export default Nav
---
+++ src/componentes/Produtos.jsx
@@ -0,0 +1,12 @@
+import { } from 'react'
+
+function Produtos() {
+
+  return (
+    <>
+    <h1>Produtos</h1>
+    </>
+  )
+}
+
+export default Produtos
